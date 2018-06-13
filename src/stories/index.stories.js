b5992282b68b92fef0189dfce078036cf9e09635
Gurnot, Frederic <Frederic.Gurnot@nytimes.com>
2018-06-12 21:33:51 -0400

    remove storybook

--- src/stories/index.stories.js
+++
@@ -1,9 +0,0 @@
-// file: src/stories/index.js
-
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import Button from '../components/button';
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={}>next</Button>);
